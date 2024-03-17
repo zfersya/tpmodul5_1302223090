8eb6fb724091010492f2b37eb7441c2b1dcc5117
zfersya <zfersya@gmail.com>
2024-03-17 16:13:42 +0700

    Menambahkan class DataGeneric

--- tpmodul5_1302223090/Program.cs
+++ tpmodul5_1302223090/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace tpmodul5_1302223090
 {
-
     public class HaloGeneric
     {
-        static void Main(string[] args)
-        {
-            string namaPraktikkan = "Ijup ";
-            SapaUser(namaPraktikkan);
-        }
         public static void SapaUser<Ting>(Ting name)
         {
             Console.WriteLine("Halo User " + name + "\n");
+        }
+    }
 
+    public class DataGeneric<Ting>
+    {
+        private Ting data { get; set; }
+        public DataGeneric(Ting data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data Yang Tersimpan Adalah: " + data);
+        }
+    }
+    class MainProgram
+    {
+        public static void Main(string[] args)
+        {
+            string namaPraktikkan = "Ijup";
+            HaloGeneric.SapaUser(namaPraktikkan);
+            DataGeneric<string> Y = new DataGeneric<string>("Y");
+            Y.PrintData();
+            DataGeneric<string> nim = new DataGeneric<string>("1302223090");
+            nim.PrintData();
         }
 
     }
